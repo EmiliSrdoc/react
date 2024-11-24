c35edcf1427969eb5b6e539696aa8db2954cab13
Emili Srdoc <emili@srdoc.si>
2024-11-24 10:13:06 +0100

    the cats broke every thing

--- src/layout/Main.tsx
+++ src/layout/Main.tsx
@@ -4,20 +4,26 @@ export default function Main() {
   return (
     <main>
       <h2>Here are some very cute cats</h2>
-      <h3>... and one very short story, it goes like this...</h3>
+      <h3>And one very short story, it goes like this...</h3>
       <p>
         One day my dogs woke up, drank water, went for a walk and saw 1, 2, 3,
         4, 5... 13 cats. it was a big drama.
       </p>
       <Gallery />
       <p>
-        they said woof woof, thow they were scared, I said “look how cute they
-        are,” but inside the cats were pure evell. We took them to our home and
+        they said woof woof, tough they were scared, I said “look how cute they
+        are,” but inside the cats were pure evil. We took them to our home and
         gave one of them a name. It was Gerry. Next was Alex and so on... The
-        next day (wednesday) I went to a building, there is an office where I
-        work
+        next day (Wednesday) I went to a building, there is an office where I
+        work.
       </p>
-      <p></p>
+      <p>
+        I worked until 1 AM, then went to buy food for the cats. When I came
+        home. The cats made a hole mess, they pushed a glass from the table,
+        pooped on the floor and played whit my diary. I was not quite happy, but
+        I thought that they did it by accident. The next day it was the same,
+        all broken. It was enough.
+      </p>
     </main>
   );
 }
